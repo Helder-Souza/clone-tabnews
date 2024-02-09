7d4e6dea41761078c14d869f032afca20db62749
Helder Souza <72743384+Helder-Souza@users.noreply.github.com>
2024-02-09 14:32:11 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>parangaricutirimirruaro ☢</h1>
+  return <h1>parangaricutirimirruaro ☢</h1>;
+}
+
+function teste() {
+  console.log("indentação errada");
 }
 
 export default Home;
